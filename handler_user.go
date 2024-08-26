72168d9cf6f37d953861f67d1c9f51827868e115
Jan Geschwandtner <geschwandtner.jan@gmail.com>
2024-08-26 23:44:30 +0200

    Implement authentication

--- handler_user.go
+++ handler_user.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"net/http"
+	"rss-aggregator/internal/auth"
 	"rss-aggregator/internal/database"
 	"time"
 )
@@ -30,6 +31,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Could not create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not find user: %v", err))
 		return
 	}
 
